59a666cba503530d11b659db0669fc3dcfd0d2bf
Hao Luo <44961008+ashipiling@users.noreply.github.com>
2019-10-09 20:59:55 +0800

    Delete 1008.py

--- 1008.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    movedata = input().split()
-    numdata = input().split()
-    new_s = numdata[int(movedata[0])-int(movedata[1]):] + numdata[: int(movedata[0])-int(movedata[1])]
-    li = ' '.join(new_s)
-    print(li, end='')
